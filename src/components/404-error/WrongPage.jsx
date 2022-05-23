46938ed03374fef07c00b83a658359db1a3d5836
Паша Горбатюк <92729703+PavloHorbatiuk@users.noreply.github.com>
2022-05-23 13:36:25 +0300

    wrong page done

--- src/components/404-error/WrongPage.jsx
+++ src/components/404-error/WrongPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './WrongStylePage.css';
+import './../qustion/QuestionsStyle.css';
+import BackCross from './../BackCross/BackCross';
 
 function WrongPage() {
 	return (
 		<div className="wrongPage">
 			<div className="container">
 				<div className="content">
-					<h1>404</h1>
+					<h2 className="num-four">404</h2>
+					<div className="title404">Ой лишенько!</div>{' '}
+					<div className="title404">🙄 Щось пішло не так :(</div>
+					<p>Cторінку було видалено системою, aбо</p>
+					<p> адресна строка містить помилки - перевірте чи</p>
+					<p>правильно ви ввели посилання</p>
+				</div>
+				<div className="btn-wrong">
+					<BackCross />
 				</div>
 			</div>
 		</div>
